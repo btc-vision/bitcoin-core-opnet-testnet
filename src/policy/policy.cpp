d3c9710e1ba6d7d35b860614c35121c3157f4c90
BlobMaster41 <96896824+BlobMaster41@users.noreply.github.com>
2024-10-24 01:01:34 -0400

    Update policy.cpp

--- src/policy/policy.cpp
+++ src/policy/policy.cpp
@@ -195,10 +195,14 @@ bool AreInputsStandard(const CTransaction& tx, const CCoinsViewCache& mapInputs)
             // validation.
             return false;
         } else if (whichType == TxoutType::SCRIPTHASH) {
+            strprintf("script hash");
+
             std::vector<std::vector<unsigned char> > stack;
             // convert the scriptSig into a stack, so we can inspect the redeemScript
-            if (!EvalScript(stack, tx.vin[i].scriptSig, SCRIPT_VERIFY_NONE, BaseSignatureChecker(), SigVersion::BASE))
+            if (!EvalScript(stack, tx.vin[i].scriptSig, SCRIPT_VERIFY_NONE, BaseSignatureChecker(), SigVersion::BASE)) {
+                 strprintf("inv script");
                 return false;
+            }
             if (stack.empty())
                 return false;
             CScript subscript(stack.back().begin(), stack.back().end());
